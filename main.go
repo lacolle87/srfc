ad4af586f4d6a7bfee81338f8874a86b2424ad61
Nikita Kuznetsov <Lacolle87@gmail.com>
2025-01-02 01:27:15 +0300

    moved main

--- main.go
+++ main.go
@@ -17,20 +17,33 @@ type RaceData struct {
 	ExtraFuel  float64
 }
 
-func (r *RaceData) calcLaps() {
+func (r *RaceData) calculateLaps() {
 	r.TotalLaps = r.RaceMin * 60 / r.LapSec
+}
+
+func (r *RaceData) calculateFuel() {
+	r.TotalFuel = float64(r.TotalLaps) * r.FuelPerLap
+}
+
+func (r *RaceData) calculateStints() {
+	r.Stints = uint(math.Ceil(r.TotalFuel / r.FuelCap))
 }
 
 func (r *RaceData) addExtraFuel() {
 	r.TotalFuel += r.FuelPerLap * r.ExtraFuel
 }
 
-func (r *RaceData) calcFuel() {
-	r.TotalFuel = float64(r.TotalLaps) * r.FuelPerLap
-}
-
-func (r *RaceData) calcStints() {
-	r.Stints = uint(math.Ceil(r.TotalFuel / r.FuelCap))
+func (r *RaceData) promptUserInput() {
+	fmt.Print("Enter lap time (in seconds): ")
+	fmt.Scan(&r.LapSec)
+	fmt.Print("Enter race length (in minutes): ")
+	fmt.Scan(&r.RaceMin)
+	fmt.Print("Enter fuel consumption per lap: ")
+	fmt.Scan(&r.FuelPerLap)
+	fmt.Print("Enter fuel tank capacity: ")
+	fmt.Scan(&r.FuelCap)
+	fmt.Print("Enter extra fuel needed in laps: ")
+	fmt.Scan(&r.ExtraFuel)
 }
 
 func main() {
@@ -40,27 +53,18 @@ func main() {
 	flag.UintVar(&raceData.RaceMin, "rl", 0, "Race length in minutes")
 	flag.Float64Var(&raceData.FuelPerLap, "fc", 0, "Fuel consumption per lap")
 	flag.Float64Var(&raceData.FuelCap, "tc", 110, "Fuel tank capacity")
-	flag.Float64Var(&raceData.ExtraFuel, "ef", 2, "Extra fuel needed, default: 2")
+	flag.Float64Var(&raceData.ExtraFuel, "ef", 2, "Extra fuel needed")
 
 	flag.Parse()
 
 	if raceData.LapSec == 0 || raceData.RaceMin == 0 || raceData.FuelPerLap == 0 {
 		fmt.Println("Some required flags are missing. Entering interactive mode...")
-		fmt.Print("Enter lap time (in seconds): ")
-		fmt.Scan(&raceData.LapSec)
-		fmt.Print("Enter race length (in minutes): ")
-		fmt.Scan(&raceData.RaceMin)
-		fmt.Print("Enter fuel consumption per lap: ")
-		fmt.Scan(&raceData.FuelPerLap)
-		fmt.Print("Enter fuel tank capacity: ")
-		fmt.Scan(&raceData.FuelCap)
-		fmt.Print("Enter extra fuel needed in laps: ")
-		fmt.Scan(&raceData.ExtraFuel)
+		raceData.promptUserInput()
 	}
 
-	raceData.calcLaps()
-	raceData.calcFuel()
-	raceData.calcStints()
+	raceData.calculateLaps()
+	raceData.calculateFuel()
+	raceData.calculateStints()
 	raceData.addExtraFuel()
 
 	fmt.Printf("Total fuel: %.2f\nTotal laps: %d\nStints: %d\n", raceData.TotalFuel, raceData.TotalLaps, raceData.Stints)
